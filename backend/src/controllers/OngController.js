2ea781624bb62df04064fc9b5ee8ddcce68270a7
Maykon Capellari <maykonluiscapellari@gmail.com>
2020-03-28 10:55:07 -0300

    Backend da applicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,15 @@
+const connection = require("../database/connection");
+const crypto = require("crypto");
+
+exports.create = async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({ id, name, email, whatsapp, city, uf });
+  return res.json({ id });
+};
+
+exports.list = async (req, res) => {
+  const ongs = await connection("ongs").select("*");
+  return res.json(ongs);
+};
